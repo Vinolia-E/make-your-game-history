3662f1d9835703431b14c2552a1bea0c9e23f23a
krodgers <kaunda.rodgers@gmail.com>
2025-02-26 14:07:31 +0300

    initial implementation

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+// Constants and Variables
+const ROWS = 20;
+const COLS = 10;
+const BLOCK_SIZE = 30;
+const SHAPES = [
+    [[1, 1, 1, 1]],
+    [[1, 1], [1, 1]],
+    [[1, 1, 0], [0, 1, 1]],
+    [[0, 1, 1], [1, 1, 0]],
+    [[1, 1, 1], [0, 1, 0]],
+    [[1, 1, 1], [1, 0, 0]],
+    [[1, 1, 1], [0, 0, 1]]
+];
+const COLORS = ['#00f0f0', '#f0f000', '#f00000', '#00f000', '#a000f0', '#f0a000', '#0000f0'];
+
+let grid, score, lives, startTime, isPaused, lastFrameTime, dropCounter, dropInterval, baseDropInterval, currentPiece, isGameOver;
+const container = document.getElementById('game-container');
+const scoreElement = document.getElementById('score');
+const timerElement = document.getElementById('timer');
+const livesElement = document.getElementById('lives');
+const pauseMenu = document.getElementById('pause-menu');
