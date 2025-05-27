902c08bb14ae72f8c8e39c4906218ba637003cd5
vandisi <vinolia.b.19@gmail.com>
2025-05-27 17:34:23 +0300

    feat: Add storyTeller function that displays the story on the pause menu

--- js/story.js
+++ js/story.js
@@ -3,7 +3,8 @@ let index = 0;
 const story = [
     "Title: ",
     "🐸 Freddy the Frog and the Jiggly Jelly Blocks", "",
-    "Deep in the heart of the Wiggly Willow Forest, there lived a happy little frog named Freddy. Freddy had one big job: to build the tallest tower in the forest using magical Jiggly Jelly Blocks!", "",
+    "Deep in the heart of the Wiggly Willow Forest, there lived a happy little frog named Freddy.","",
+     "Freddy had one big job: to build the tallest tower in the forest using magical Jiggly Jelly Blocks!", "",
     "These jelly blocks weren’t just ordinary blocks — they wobbled, bounced, and giggled every time Freddy stacked them just right. But if he stacked them wrong… SPLAT! They flopped over like pancakes!", "",
     "One morning, Freddy woke up to a big surprise: The Jelly Fairy had dropped jelly blocks everywhere! Trees were wearing jelly hats, squirrels were stuck inside jelly cubes, and even the sun looked a little wobbly","",
     "“Oh jellybeans!” said Freddy. “I need help stacking them back into a super silly tower!”",
@@ -11,8 +12,13 @@ const story = [
     "Can you help Freddy bring bounce and giggles back to Wiggly Willow Forest? 🍓💫🎉", ""
 ];
 
-// shows the pop-up for increased tetro speed
-function showStoryNotification(a) {
+// let totalPausedTime = 0;
+// let pauseStartTime = 0;
+// shows the pop-up for story
+function showStoryNotification() {
+
+    // isPaused = true;
+    gamePausedForStory= true; // Set the game state to paused for story
     const storynotification = document.createElement('div');
     storynotification.textContent = story[index];
     storynotification.className = story.title;
@@ -21,7 +27,27 @@ function showStoryNotification(a) {
     
     setTimeout(() => {
         document.body.removeChild(storynotification);
+        gamePausedForStory = true;
     }, 5000);
+    index++;
+    if (index >= story.length) {
+        index = 0; // Reset index to loop through the story
+    }
+}
+
+// Stops the game and displays the story.
+// Shows the pause menu with the story.
+function storyTeller() {
+    gamePausedForStory = true;
+    isPaused = true;
+    pauseMenu.style.display = 'block';
+
+    // document.body.removeChild(storynotification);
+    const storynotification = document.createElement('div');
+    storynotification.textContent = story[index];
+    storynotification.style.marginBottom = '10px';
+    storynotification.className = 'story-notification';
+    pauseMenu.insertBefore(storynotification, pauseMenu.firstChild);
     index++;
     if (index >= story.length) {
         index = 0; // Reset index to loop through the story
